428147bc2394be62e099302b7198bb63740e7e5b
mtraina <matteo.traina.mail@gmail.com>
2019-12-28 16:50:24 +0100

    project init

---
+++ src/test/java/org/mtraina/quickstart/NativeGreetingResourceIT.java
@@ -0,0 +1,9 @@
+package org.mtraina.quickstart;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeGreetingResourceIT extends GreetingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
