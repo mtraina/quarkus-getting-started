18003940e62e733cb925e96e7c615288c9ee510f
mtraina <matteo.traina.mail@gmail.com>
2019-12-28 17:22:20 +0100

    async call added

--- src/main/java/org/mtraina/quickstart/GreetingResource.java
+++ src/main/java/org/mtraina/quickstart/GreetingResource.java
@@ -6,6 +6,8 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
 
 @Path("/hello")
 public class GreetingResource {
@@ -26,7 +28,9 @@ public class GreetingResource {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "hello";
+    public CompletionStage<String> hello() {
+        return CompletableFuture.supplyAsync(() -> {
+            return "hello";
+        });
     }
 }
