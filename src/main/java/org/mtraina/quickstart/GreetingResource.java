acf7782c872b985c46791c4492782e17f38472ec
mtraina <matteo.traina.mail@gmail.com>
2019-12-28 17:20:26 +0100

    service dep added

--- src/main/java/org/mtraina/quickstart/GreetingResource.java
+++ src/main/java/org/mtraina/quickstart/GreetingResource.java
@@ -1,12 +1,28 @@
 package org.mtraina.quickstart;
 
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 @Path("/hello")
 public class GreetingResource {
+
+    private final GreetingService greetingService;
+
+    @Inject
+    public GreetingResource(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/greeting/{name}")
+    public String greeting(@PathParam("name") String name) {
+        return greetingService.greeting(name);
+    }
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
